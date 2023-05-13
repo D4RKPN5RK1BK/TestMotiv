1798192f3ac190f05ac0c2316b84bc6d2da7de39
Исаков Павел <pavel_i@holdingbp.ru>
2023-05-13 22:04:06 +0500

    пакеты

--- TestMotiv/Global.asax.cs
+++ TestMotiv/Global.asax.cs
@@ -13,7 +13,6 @@ namespace TestMotiv
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            
             UnityConfig.RegisterComponents();
         }
     }
